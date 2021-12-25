eb88d060fb29718ad0c4c0228af7ddea47ae4f84
xsy <xsy15282426242@163.com>
2021-12-25 13:32:04 +0800

    希尔排序纠正

--- array/src/main/java/com/shaw/org/day04/HellSort.java
+++ array/src/main/java/com/shaw/org/day04/HellSort.java
@@ -11,18 +11,12 @@ import java.util.Arrays;
  */
 public class HellSort {
     public static void main(String[] args) {
-        int[] arr = {8, 9, 1, 7, 2, 3, 10,5, 4, 6, 0};
+        int[] arr = {8, 9, 1, 7, 2, 3, 10, 5, 4, 6, 0};
         //分步骤
         //hellSort(arr);
 //        hellSortAll(arr);
-//        upgradeHellSort2(arr);
+        upgradeHellSort(arr);
 //        int count = 1;
-//        do{
-//            count++;
-//        }while(count<6);
-//        System.out.println(count);
-        BigDecimal d = new BigDecimal("-1");
-        System.out.println(BigDecimal.ZERO.subtract(d));
     }
 
     // 希尔排序--> 思路分析: 以组为单位进行插入排序两种实现，第一种交换法，第二种简单插入法
@@ -56,33 +50,34 @@ public class HellSort {
 
                 temp = arr[i];
                 for (int j = i - group; j >= 0; j -= group) {
-                    if (arr[j] > arr[j+group]) {
-                        arr[j+group] = arr[j];
+                    if (temp < arr[j]) {
+                        arr[j + group] = arr[j];
                         cur -= group;
                     }
                 }
-                if(cur!=i){
+//                if (cur != i) {
                     arr[cur] = temp;
-                }
+//                }
             }
-            group = group /2;
+            group = group / 2;
         }
         System.out.println(Arrays.toString(arr));
     }
+
     private static void upgradeHellSort2(int arr[]) {
-        int temp = 0;
+//        int temp = 0;
         int group = arr.length / 2;
         while (group != 0) {
-            for (int i = 0; i < arr.length; i++) {
+            for (int i = group; i < arr.length; i++) {
                 int cur = i;
-                temp = arr[i];
-                while(i-group>=0 && temp>arr[i-group]){
-                    arr[i] = arr[i-group];
+                int temp = arr[i];
+                while (cur-group >= 0 && temp < arr[cur-group]){
+                    arr[cur] = arr[cur-group];
                     cur -= group;
                 }
-                arr[cur+group] = temp;
+                arr[cur] = temp;
             }
-            group = group /2;
+            group /= 2;
         }
         System.out.println(Arrays.toString(arr));
     }
